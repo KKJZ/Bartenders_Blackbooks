045957af98e69fb57aab2f3932ea4575b5f07879
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-07-25 13:36:14 -0700

    Able to get random drink

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 app.use(morgan('common'));
 
 app.get('/', (req,res) => {
-	res.send('Working');
+	request('https://www.thecocktaildb.com/api/json/v1/1/random.php', function(err, _res, body) {
+		const drinks = JSON.parse(body);
+		const drink = drinks.drinks[0];
+		console.log(drink);
+		res.send(drink.strDrink);
+	});
 });
 
 app.listen(5000, () => console.log('listening at port 5000'));
