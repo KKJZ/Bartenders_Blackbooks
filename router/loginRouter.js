674b388846a6016061d3a3e7f90d1f6b2c8e10da
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-08-01 18:45:14 -0700

    started work on login

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -5,6 +5,19 @@ const {DrinkCollection} = require('../models/drinks');
 const {Users} = require('../models/users');
 
 //login request would return a jwt
-// app.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+	const requiredFields = ['userName', 'password'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i]
+		if (!(field in req.body)) {
+			const messge = `Request body is missing ${field}`
+			console.error(messge);
+			return res.status(400).send(messge);
+		}
+	}
+	let user = Users();
+	let {userName, password} = req.body;
+	user.validatePassword(password)
+});
 
 module.exports = router;
