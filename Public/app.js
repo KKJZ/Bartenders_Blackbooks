012156ee98e3e7dba5d81949b10ea8b82c41a77c
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-08-02 20:47:54 -0700

    Working on ajax calls

--- Public/app.js
+++ Public/app.js
@@ -1,9 +1,8 @@
-const randomDrinkURL = 'http://localhost:5000/random'
-
-//handle the random button
+//handle the get drink button
 function handleButton () {
-	$('.random').on('click', function () {
+	$('.drinks').on('click', function () {
 		event.preventDefault();
+		console.log('button pressed GETting drinks');
 		getDrink(renderDrink);
 	})
 }
@@ -11,7 +10,7 @@ function handleButton () {
 //ajax call to the random drink endpoint
 function getDrink (callback) {
 	const options = {
-		url: randomDrinkURL,
+		url: "/drinks",
 		type: 'GET',
 		dataType: 'json',
 		success: callback,
@@ -23,19 +22,107 @@ function getDrink (callback) {
 
 //render drink  to the div.results
 function renderDrink (obj) {
-	const options = `
-	<article>
-	<img class="result" src=${obj.thumbnail}>
-	Category: ${obj.category}<br>
-	Name : ${obj.name}<br>
-	Glass: ${obj.glass}<br>
-	</article>`
-	$('div.results').html(options)
+	for (let i=0; i<obj.length; i++) {
+		const options = `
+		<article>
+		<img class="result" src=${obj[i].drinkImage}>
+		User: ${obj[i].user}<br>
+		Name : ${obj[i].drinkName}<br>
+		Glass: ${obj[i].glass}<br>
+		</article>`
+		console.log(obj[i]);
+		$('div.drink_results').append(options);
+	};
 }
 
 //for fail function
 function forFail (err) {
 	console.log(err);
+	$('div.drink_results').html(`<p>ERROR: code ${err.status},<br>${err.responseText}`)
 }
 
+//make user
+function handleUser () {
+	$('#user_register').on('submit', function() {
+		event.preventDefault();
+		let form = $(this);
+		console.log(form);
+		console.log('User submitted.');
+		let data = {
+			userName: form[0][1].value,
+			password: form[0][2].value,
+			email: form[0][3].value	
+		}
+		console.log(data);
+		postUser(data, renderUser);
+	})
+}
+
+//post user makes a post request to the server to make the user
+function postUser (data, callback) {
+	console.log(data)
+	const request = {
+		url: '/users',
+		type: 'POST',
+		dataType: 'json',
+		data: JSON.stringify(data),
+		contentType: "application/json; charset=utf-8",
+		success: callback,
+		error: errorUser,
+	}
+	$.ajax(request);
+};
+
+//render user returns the user name and a success status back to the user
+function renderUser (obj) {
+	console.log(obj);
+	$('div.user_results').html(`${obj.userName} was successfully created.`)
+};
+
+//error on user make 
+function errorUser (err) {
+	console.log(err.responseText, err.status);
+	$('div.user_results').html(`<p>ERROR: code ${err.status},<br>${err.responseText}`)
+};
+
+//handle form drink
+function handleMakeDrink () {
+	$('#drink_register').on('submit', function() {
+		event.preventDefault();
+		let form = $(this);
+		console.log(`POSTing drink data`);
+		let data = {
+			user: "test",
+			drinkName: form[0][1].value,
+			glass: form[0][2].value,
+			ingredents: [{
+				ingredent: form[0][3].value,
+				measurement: form[0][4].value
+			}],
+			instructions: form[0][5].value,
+			drinkImage: form[0][6].value
+		};
+		console.log(form);
+		console.log(data);
+	})
+};
+
+//post drink to db
+function postDrink() {
+
+};
+
+//return drink from db
+function renderMadeDrink () {
+
+};
+
+//handle error
+function makeDrinkError (err) {
+	console.log(err.responseText, err.status);
+	$('div.user_results').html(`<p>ERROR: code ${err.status},<br>${err.responseText}`);
+};
+
 handleButton();
+handleUser();
+handleMakeDrink();
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -18,6 +18,7 @@ router.get('/', (req, res) => {
 
 //make user profile
 router.post('/', (req, res) => {
+	console.log(req.body);
 	const requiredFields = ['userName', 'password', 'email'];
 	for (let i=0; i<requiredFields.length; i++) {
 		const field = requiredFields[i];
